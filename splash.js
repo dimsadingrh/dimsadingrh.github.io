010e00fe87b5ee1bbfa764591401ed9f0610c256
Dimas Adi Nugroho <90830987+dimsadingrh@users.noreply.github.com>
2025-06-02 00:10:28 +0700

    card activity, new class, fixing the card size

--- splash.js
+++ splash.js
@@ -2,24 +2,22 @@ window.addEventListener('DOMContentLoaded', function() {
   const splash = document.getElementById('splash');
   if (!splash) return;
 
-  // Ambil semua card dan header
-  const cards = document.querySelectorAll('.card');
+  // Pilih semua card dengan class baru
+  const cards = document.querySelectorAll('.hello-there, .profile-photo, .activity, .project, .social, .contact');
   const header = document.querySelector('.header');
 
   setTimeout(() => {
     splash.classList.add('splash-hide');
 
-    // Trigger animasi scale up pada header dan card
     setTimeout(() => {
       header.classList.add('animated');
       cards.forEach((card, i) => {
         setTimeout(() => {
           card.classList.add('animated');
-        }, i * 20); // efek stagger
+        }, i * 20);
       });
-    }, 50); // Mulai animasi saat splash mulai slide up
+    }, 50);
 
-    // Setelah animasi selesai, hapus splash dari DOM
     setTimeout(() => {
       splash.style.display = 'none';
     }, 600);
